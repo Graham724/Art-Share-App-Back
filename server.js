050a0dade6439f32e390917cc43063f98d5fda10
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-10-19 19:10:49 -0500

    server.js set up

--- server.js
+++ server.js
@@ -8,3 +8,27 @@ const cors = require('cors')
 const app = express()
 
 
+// middleware
+app.use(express.json())
+app.use(cors())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// routes
+app.use('/api/artwork', artworkRoutes)
+
+
+// connect to mongoose
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('DB is connected listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
